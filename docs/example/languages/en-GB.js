93668e45123cb1cf3dc2b36a340c032ca7a5701b
Edazpotato <edazpotato@gmail.com>
2021-07-11 19:17:37 +1200

    e

--- docs/example/languages/en-GB.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var senutila_1 = require("senutila");
-exports.default = new senutila_1.Language("en-GB", function (bot) { return ({
-    PING_COMMAND_RESPONSE_TEXT: "Pong!",
-    PING_COMMAND_DESCRIPTION: "Says pong to you!",
-}); });
